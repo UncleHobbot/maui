c8cb36632d3cdf72b86336eed689f575ace665de
Max Iliashenko <uncle.ho.max@gmail.com>
2021-02-23 11:56:55 -0500

    Add project files.

---
+++ BPMS/BPMSApp.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Platform;
+using Xamarin.Platform.Core;
+
+namespace BpmsPOC
+{
+	public class BPMSApp : IApp
+	{
+		public BPMSApp()
+		{
+			Platform.Init();
+		}
+
+		public IView CreateView()
+		{
+			var verticalStack = new VerticalStackLayout { Spacing = 5, BackgroundColor = Color.AntiqueWhite, VerticalOptions = LayoutOptions.Fill };
+			var header = new HorizontalStackLayout { Spacing = 2, BackgroundColor = Color.LightGreen, VerticalOptions = LayoutOptions.Start };
+			var field = new HorizontalStackLayout { Spacing = 2, VerticalOptions = LayoutOptions.CenterAndExpand };
+			var footer = new HorizontalStackLayout { Spacing = 2, VerticalOptions = LayoutOptions.End };
+
+			var btnTasks = new Button { Text = "Tasks" };
+			btnTasks.Clicked += (sender, args) =>
+			{
+				//ClearLayout(field);
+				field.Add(new Label { Text = "Task List" });
+			};
+
+			var btnLog = new Button { Text = "Log" };
+			btnLog.Clicked += (sender, args) =>
+			{
+				//ClearLayout(field);
+				var lbl = new Label { Text = "Application Log", TextColor = Color.Black };
+				field.Add(lbl);
+				lbl.IsVisible = false;
+				lbl.IsVisible = true;
+			};
+
+			var btnForm = new Button { Text = "Form" };
+			btnForm.Clicked += (sender, args) =>
+			{
+				//ClearLayout(field);
+				field.Add(new Label { Text = "User Form" });
+			};
+
+			header.Add(btnTasks);
+			header.Add(btnLog);
+			header.Add(btnForm);
+
+			var lblField = new Label { Text = "This is a field" };
+			field.Add(lblField);
+
+			var label = new Label { Text = "Powered by BPMS" };
+			footer.Add(label);
+
+			verticalStack.Add(header);
+			verticalStack.Add(field);
+			verticalStack.Add(footer);
+
+			return verticalStack;
+		}
+
+		private void ClearLayout(IStackLayout l)
+		{
+			var cs = l.Children.ToList();
+			foreach (var child in cs)
+				l.Remove(child);
+		}
+	}
+}
---
+++ src/Platform.Handlers/samples/Sample.Droid/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using BpmsPOC;
+using Xamarin.Platform;
+
+namespace Sample.Droid
+{
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		ViewGroup _page;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+			SetContentView(Resource.Layout.activity_main);
+
+			AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);
+
+			_page = FindViewById<ViewGroup>(Resource.Id.Page);
+
+			var app = new BPMSApp();
+
+			var view = app.CreateView();
+
+			Add(view);
+		}
+
+		void Add(params IView[] views)
+		{
+			foreach (var view in views)
+			{
+				_page.AddView(view.ToNative(this), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent , ViewGroup.LayoutParams.MatchParent));
+			}
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+}
